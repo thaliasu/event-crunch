8e174c88e08c920b6f1a60a64f380bb4a8f794ca
Quinten O'Neal <qtylero@gmail.com>
2018-04-11 09:12:38 -0400

    04-11-2018
    basic front end with inputs and both APIs working

--- js/script.js
+++ js/script.js
@@ -1,45 +1,35 @@
 
-var key = '0d450de0d245dfc5e948f81653ccd3f7';
+var weatherKey = '0d450de0d245dfc5e948f81653ccd3f7';
+var bandKey = '4f45c5ceb4c55ae3533d6bff4bdbfd0e';
 var arr = []; 
+var bandData;
+var locationData;
 
 $(document).ready(function() {
-
 	$("#get-weather").click(function(e){
 		e.preventDefault();
-		var searchText = $("#search").val();
-		$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+searchText+",us&units=imperial&APPID=0d450de0d245dfc5e948f81653ccd3f7", 
-		function(data){
-			var weatherData = {
-				temperature: document.getElementById("temp"),
-				temperatureValue: 0,
-				units: "°F"
-			};
-			$("#weather").html("<p>it is currently <a href='#'><span id='temp'>"+data.main.temp+weatherData.units+"</span></a> in "+searchText+"</p><br /><p>"+data.weather[0].description+"<img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'></p>");
-			
-			$("#temp").click(function(){
-				
-				function switchUnits(){
-				  if (weatherData.units == "°C"){
-				    weatherData.temperatureValue = roundTemperature(weatherData.temperatureValue * 9/5 + 32);
-				    weatherData.units = "°F";
-				  }
-				  else{
-				    weatherData.temperatureValue = roundTemperature((weatherData.temperatureValue -  32) * 5/9);
-				    weatherData.units = "°C";
-				  }
-
-				  weatherData.temperature.innerHTML = weatherData.temperatureValue + weatherData.units + ", ";      
-				}
+		//prevent empty fields
+	   if($.trim($('#artist-search').val()) == ''){
+	      $(".error").css("display", "block").html("input cannot be left blank");
+	   } else {
+			$(".error").css("display", "none");
+			//artist field
+			var artistField = $("#artist-search").val();
+			$.getJSON("https://rest.bandsintown.com/artists/"+artistField+"/events?app_id="+bandKey, function(response){
+				console.log(response);
+				bandData = response;
 			});
-			
-			$.getJSON("https://api.flickr.com/services/feeds/photos_public.gne?tags="+searchText+",+skyline&tagmode=all&format=json&jsoncallback=?", function(response){
-				console.log(response.items[1].media.m);
-				$("body").css('background-image', 'url('+response.items[2].media.m+')');
-				$("#image-source").attr("href", response.items[1].media.m);
-				//$("#weather").css('background-image', 'url('+response.media.m[1]+')');
+			//location field
+			var locationField = $("#location").val();
+			$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+locationField+",us&units=imperial&APPID="+weatherKey, 
+			function(data){
+				console.log(data);
+				locationData = data;
 			});
-		});
-
-	})
+			$(".info-container").html("<div class='row'><div class='col-1-2'><h1>"+bandData.venue.city[locationField]+"</h1>");
+		}
+	});
+});
 	
-  });
+	
+  
