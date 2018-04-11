e89672bda5ec956a1df31f43675f0fd7f4e9b8b5
Quinten O'Neal <qtylero@gmail.com>
2018-04-11 12:30:21 -0400

    Add files via upload
    currently global variables "bandData" and "locationData" are coming back undefined.. therefore the "getEventByCity" function on line 64 is not working properly.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 var weatherKey = '0d450de0d245dfc5e948f81653ccd3f7';
 var bandKey = '4f45c5ceb4c55ae3533d6bff4bdbfd0e';
-var arr = []; 
+
 var bandData;
 var locationData;
 
@@ -19,6 +19,21 @@ $(document).ready(function() {
 				console.log(response);
 				bandData = response;
 			});
+
+			/*$.ajax({
+
+	            url: 'https://rest.bandsintown.com/artists/'+artistField+'/events?app_id='+bandKey,
+	            type: 'GET',
+	           
+	            async: false,
+	            success: function(data){
+	              	console.log(data);
+					bandData = data;
+	            },
+	            error: function(response){
+	              console.log('error!');
+	            }
+          	});*/ //testing different ajax methods
 			//location field
 			var locationField = $("#location").val();
 			$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+locationField+",us&units=imperial&APPID="+weatherKey, 
@@ -26,10 +41,40 @@ $(document).ready(function() {
 				console.log(data);
 				locationData = data;
 			});
-			$(".info-container").html("<div class='row'><div class='col-1-2'><h1>"+bandData.venue.city[locationField]+"</h1>");
-		}
+			/*$.ajax({
+	            url: 'http://api.openweathermap.org/data/2.5/weather?q='+locationField+',us&units=imperial&APPID='+weatherKey,
+	            type: 'GET',
+	            
+	            async: false,
+	            success: function(data){
+	              	console.log(data);
+					locationData = data;
+	            },
+	            error: function(response){
+	              console.log('error!');
+	            }
+          	});*/ //testing different ajax methods
+
+			/*function callback(response, data) {
+				bandData = response;
+				locationData = data;
+			}*/
+
+			
+			function getEventsByCity(city) {
+			  	return bandData.filter(
+			      function(bandData){ return bandData.city == city }
+			  );
+			};
+		
+			var found = getEventsByCity(locationField);
+			};
+
+		});
 	});
-});
+
+	
+
 	
 	
   
