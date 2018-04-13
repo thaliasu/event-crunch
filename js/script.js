42129d00693c47a18290d0dd3c6ff56dd1bc3b1e
Quinten O'Neal <qtylero@gmail.com>
2018-04-13 16:03:14 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,26 +3,43 @@ var weatherKey = '0d450de0d245dfc5e948f81653ccd3f7';
 var bandKey = '4f45c5ceb4c55ae3533d6bff4bdbfd0e';
 
 $(document).ready(function() {
+	$('.button-collapse').sideNav();
+    $('.parallax').parallax();
+	
 	$("#get-weather").click(function(e){
 		e.preventDefault();
+		
 		//prevent empty fields
 	   if($.trim($('#artist-search').val()) == ''){
 	      $(".error").css("display", "block").html("input cannot be left blank");
 	   } else {
 			$(".error").css("display", "none");
+			
 			//artist field
 			var artistField = $("#artist-search").val();
 			$.getJSON("https://rest.bandsintown.com/artists/"+artistField+"/events?app_id="+bandKey, function(response){
+				console.log(response);
 				var FilteredData = response.filter(function(elem){return (elem.venue.city == locationField) });
+						
                       if(FilteredData.length == 1){
-                          console.log(FilteredData.length);
-                          $(".artist-header").html(FilteredData[0].lineup)
+
+                        //$(".info-container").html("<h1 class='artist-header'>"+FilteredData[0].lineup+"</h1><div class='event-info'><p class='date'>"+FilteredData[0].datetime+"</p><p class='venue'>"+FilteredData[0].venue.name+"</p><p class='desc'>"+FilteredData[0].description+"</p></div>");
+                        $(".artist-header").html(FilteredData[0].lineup);
+                        $(".date").html(FilteredData[0].datetime);
+                        $(".venue").html(FilteredData[0].venue.name);
                       }
                       else if (FilteredData.length > 1) {
-                          for (var i = 0; i < FilteredData.length; i++) {
-                          	console.log(FilteredData[i].lineup);
-                          }
+                          $.each(FilteredData, function(i, evt){
+                          	$(".info-container").html("<h1 class='artist-header'>"+evt.lineup+"</h1><div class='event-info'><p class='date'>"+evt.datetime+"</p><p class='venue'>"+evt.venue.name+"</p></div>");
+                          	if (i == FilteredData.length) {
+                          		return false;
+                          	}
+
+                          }) 
+                      } else {
+                      	$(".info-container").html("<p>no data</p>");
                       }
+                      console.log(FilteredData);
 			});
 			//location field
 			var locationField = $("#location").val();
@@ -31,7 +48,6 @@ $(document).ready(function() {
 				
 			});
 			};
-
 		});
 	});
 
