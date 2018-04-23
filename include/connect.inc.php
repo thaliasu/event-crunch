7b8c638b7995969b2e61ea5d98b570d9e66bcb81
Thalia Su <thalia989231@knights.ucf.edu>
2018-04-23 16:01:06 -0400

    Connect.inc.php

--- include/connect.inc.php
+++ include/connect.inc.php
@@ -1,9 +1,14 @@
 <?php
   //connects to Event Crunch's database
+  $username = "th402780";
+  $password = "midKnight6^";
+  $dbname = "th402780";
+
+  /*For local testing:
   $username = "root";
   $password = "";
-  $dbname = "th402780";
-  
+  $dbname = "th402780";*/
+
   $conn = mysqli_connect("localhost", "$username", "$password", "$dbname") or die(mysqli_connect_error());
 
   /*Test connection, if doubtful:
