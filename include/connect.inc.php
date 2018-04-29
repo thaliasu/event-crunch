b186a9ad8462197f0b6c7d5a871479f361898235
Tanisha9205 <33459738+Tanisha9205@users.noreply.github.com>
2018-04-28 21:35:20 -0400

    Updated user and pass to connect to database

--- include/connect.inc.php
+++ include/connect.inc.php
@@ -1,7 +1,7 @@
 <?php
   //connects to Event Crunch's database
-  $username = "root";
-  $password = "";
+  $username = "th402780";
+  $password = "midKnight6^";
   $dbname = "th402780";
 
   /*For local testing:
