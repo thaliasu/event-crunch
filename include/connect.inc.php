00db9616bcfbb6058ac4772826cbf99e8e61d189
thaliasu <thalia989231@knights.ucf.edu>
2019-02-05 17:38:41 -0500

    Connect to Postgres

--- include/connect.inc.php
+++ include/connect.inc.php
@@ -1,9 +1,9 @@
 <?php
   //connects to Event Crunch's database
-  $host = "ec2-54-83-17-151.compute-1.amazonaws.com";
+  /*$host = "ec2-54-83-17-151.compute-1.amazonaws.com";
   $username = "uqmzndwrlxrmnt";
   $password = "420f3dd0b88596486eb510637051e2b66706093b9dfb60add35a9e36883fd295";
-  $dbname = "db1begjoogpmj9";
+  $dbname = "db1begjoogpmj9";*/
 
   //For local testing:
   /*$host = "localhost";
@@ -11,7 +11,7 @@
   $password = "";
   $dbname = "event-crunch";*/
 
-  $conn = mysqli_connect("$host", "$username", "$password", "$dbname") or die(mysqli_connect_error());
+  $conn = pg_connect("host=ec2-54-83-17-151.compute-1.amazonaws.com port=5432 dbname=db1begjoogpmj9 user=uqmzndwrlxrmnt password=420f3dd0b88596486eb510637051e2b66706093b9dfb60add35a9e36883fd295") or die("Could not connect");
 
   /*Test connection, if doubtful:
   Test to insert data into table using connect.inc.php successful
